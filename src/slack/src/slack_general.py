1d954aeeb40e19364b50744be53ac92c4e6d09b5
nekotouma0114 <nekotouma0114@gmail.com>
2020-06-27 10:25:23 +0900

    [Add] Slack利用のための基底Classの作成

---
+++ src/slack/src/slack_general.py
@@ -0,0 +1,25 @@
+import json
+
+class SlackGeneral(object):
+    """
+        Slack API共通の機能を持つ基底クラス
+    """
+
+    def __init__(self,token_type: str,token_file: str) -> None:
+        """
+            SlackのTokenを読み込む
+            Parameter:
+                type:   token_fileから読み込むtokenの種類。
+                        "user" => Tokenを読み込み
+                        "bot"  => BotTokenを読み込み
+                token_file: SlackのToken情報が記載されたjsonファイルのパス
+        """
+        allow_token_types = ["bot","user"]
+
+        with open(token_file,"r") as fp:
+            config = json.load(fp)
+        
+        if token_type not in allow_token_types:
+            raise ValueError("'token_type' isnt allow value. allow value => {}".format(__allow_token_type))
+
+        self.token = config["Token" if token_type == "user" else "BotToken"]
---
+++ src/slack/tests/test_slack_general.py
@@ -0,0 +1,1 @@
+../SlackToken.json
